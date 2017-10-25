c7220e7d5237bd803b1ded960f293f1e3d0441fd
inti0455 <inti0455@192.168.1.181>
2017-10-25 10:33:40 +0200

    Denis : Kevien, t'es nul

--- 02_TP_JSF_EJB_JPA/src/fr/adaming/model/Agent.java
+++ 02_TP_JSF_EJB_JPA/src/fr/adaming/model/Agent.java
@@ -14,64 +14,71 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="agents")
+@Table(name = "agents")
 public class Agent implements Serializable {
 
-		@Id
-		@GeneratedValue(strategy=GenerationType.IDENTITY)
-		@Column(name="id_agent")
-		private int id;
-		private String mail;
-		private String mdp;
-		
-		@OneToMany(mappedBy="agent",cascade=CascadeType.ALL,fetch=FetchType.EAGER)
-		private List<Client> listeClients;
-		
-		
-		public Agent() {
-			super();
-		}
-		public Agent(String mail, String mdp) {
-			super();
-			this.mail = mail;
-			this.mdp = mdp;
-		}
-		public Agent(int id, String mail, String mdp) {
-			super();
-			this.id = id;
-			this.mail = mail;
-			this.mdp = mdp;
-		}
-		public int getId() {
-			return id;
-		}
-		public void setId(int id) {
-			this.id = id;
-		}
-		public String getMail() {
-			return mail;
-		}
-		public void setMail(String mail) {
-			this.mail = mail;
-		}
-		public String getMdp() {
-			return mdp;
-		}
-		public void setMdp(String mdp) {
-			this.mdp = mdp;
-		}
-		public List<Client> getListeClients() {
-			return listeClients;
-		}
-		public void setListeClients(List<Client> listeClients) {
-			this.listeClients = listeClients;
-		}
-		
-		@Override
-		public String toString() {
-			return "Agent [id=" + id + ", mail=" + mail + ", mdp=" + mdp + "]";
-		}
-		
-		
-		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_agent")
+	private int id;
+	private String mail;
+	private String mdp;
+
+	@OneToMany(mappedBy = "agent", cascade = CascadeType.ALL, fetch = FetchType.EAGER) // Cette ligne de code ne marche pas !
+	private List<Client> listeClients;
+
+	public Agent() {
+		super();
+	}
+
+	public Agent(String mail, String mdp) {
+		super();
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	public Agent(int id, String mail, String mdp) {
+		super();
+		this.id = id;
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public List<Client> getListeClients() {
+		return listeClients;
+	}
+
+	public void setListeClients(List<Client> listeClients) {
+		this.listeClients = listeClients;
+	}
+
+	@Override
+	public String toString() {
+		return "Agent [id=" + id + ", mail=" + mail + ", mdp=" + mdp + "]";
+	}
+
 }
