33a8a4cd81cadccb4bfecc4ea31a7aa92b2b409b
inti0455 <inti0455@192.168.1.181>
2017-10-25 13:24:40 +0200

    Denis

--- 10_ProjetGit/src/fr/adaming/dao/ICategorieDao.java
+++ 10_ProjetGit/src/fr/adaming/dao/ICategorieDao.java
@@ -1,5 +1,14 @@
 package fr.adaming.dao;
 
-public class ICategorieDao {
+import java.util.List;
 
+import fr.adaming.model.Categorie;
+import fr.adaming.model.Produit;
+
+public interface ICategorieDao {
+	public Produit addCategorie(Categorie categorie);
+	public Produit updateCategorie(Categorie categorie);
+	public boolean deleteCategorie(Categorie categorie);
+	public Produit getCategorie(Categorie categorie);
+	public List<Produit> getAllCategorie();
 }
