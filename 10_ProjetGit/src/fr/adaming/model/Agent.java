495c6d7fa9c27a0c64166e9962eae82d1533e91b
inti-0257 <inti-0257@192.168.1.167>
2017-10-25 14:13:22 +0200

    Kevin: ajout des get/set et constructeurs sur Agent

--- 10_ProjetGit/src/fr/adaming/model/Agent.java
+++ 10_ProjetGit/src/fr/adaming/model/Agent.java
@@ -18,5 +18,50 @@ public class Agent implements Serializable {
 	private int id;
 	private String mail;
 	private String password;
+	public Agent() {
+		super();
+	}
+	
+	public Agent(String mail, String password) {
+		super();
+		this.mail = mail;
+		this.password = password;
+	}
+	
+	public Agent(int id, String mail, String password) {
+		super();
+		this.id = id;
+		this.mail = mail;
+		this.password = password;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Agent [id=" + id + ", mail=" + mail + ", password=" + password + "]";
+	}
 	
 }
