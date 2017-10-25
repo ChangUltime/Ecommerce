603e6915f34bc9518904f78ee74a2bfc125e7a5b
inti0455 <inti0455@192.168.1.181>
2017-10-25 15:55:13 +0200

    Denis : Méthode add categorie

---
+++ 10_ProjetGit/src/fr/adaming/dao/CategorieDaoImpl.java
@@ -0,0 +1,54 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import fr.adaming.model.Categorie;
+
+public class CategorieDaoImpl implements ICategorieDao {
+
+	@PersistenceContext(unitName = "PU")
+	private EntityManager em;
+
+	
+	@Override
+	public Categorie addCategorie(Categorie categorie) {
+		em.persist(categorie);
+		return categorie;
+	}
+
+	@Override
+	public Categorie updateCategorie(Categorie categorie) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean deleteCategorie(Categorie categorie) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Categorie getCategorie(Categorie categorie) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Categorie> getAllCategorie() {
+		// Construction de la requete
+		String req = "SELECT cat FROM Categorie cat";
+
+		Query query = em.createQuery(req);
+
+		List<Categorie> listeCat = query.getResultList();
+
+		return listeCat;
+
+	}
+
+}
