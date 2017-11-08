e283bad7b67d99ab38cad30cb8035143df72153f
inti0455 <inti0455@DESKTOP-86BVJ6I>
2017-11-08 22:32:23 +0100

    Denis : commit produit et categorie dao

--- 11_eCommerce_JSF_Spring_Hibernate/src/main/java/fr/adaming/dao/CategorieDaoImpl.java
+++ 11_eCommerce_JSF_Spring_Hibernate/src/main/java/fr/adaming/dao/CategorieDaoImpl.java
@@ -2,10 +2,10 @@ package fr.adaming.dao;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import fr.adaming.model.Categorie;
@@ -13,45 +13,38 @@ import fr.adaming.model.Categorie;
 @Repository
 public class CategorieDaoImpl implements ICategorieDao {
 
-	@PersistenceContext(unitName = "PU")
-	private EntityManager em;
+	@Autowired
+	private SessionFactory sf;
+
+	public void setSf(SessionFactory sf) {
+		this.sf = sf;
+	}
 
 	@Override
 	public Categorie addCategorie(Categorie categorie) {
-		em.persist(categorie);
+		Session s = sf.getCurrentSession();
+		
+		s.save(categorie);
+		
 		return categorie;
 	}
 
 	@Override
 	public Categorie updateCategorie(Categorie categorie) {
-//
-//		if(getClient(client)!=null){
-//			em.merge(client);
-//			System.out.println("Client modifié: "+client);
-//			return client;
-//		} else {
-//			return null;
-//		}
-//		
-//		
-//		Categorie cateUpdate = em.find(Categorie.class, categorie.getIdCategorie());
-//		System.out.println("-----------------------" + cateUpdate);
-		try {
-			if (getCategorie(categorie)!=null) {
-				em.merge(categorie);
-				return categorie;
-			}
-		} catch (Exception e) {
-			return null;
-		}
-		return null;
+		Session s = sf.getCurrentSession();
+		
+		s.update(categorie);
+		
+		return categorie;
 	}
 
 	@Override
 	public boolean deleteCategorie(Categorie categorie) {
+		Session s = sf.getCurrentSession();
+		
 		String req = "DELETE from Categorie cate WHERE cate.idCategorie=:pIdCat";
 
-		Query query = em.createQuery(req);
+		Query query = s.createQuery(req);
 		query.setParameter("pIdCat", categorie.getIdCategorie());
 
 		int verif = query.executeUpdate();
@@ -65,25 +58,28 @@ public class CategorieDaoImpl implements ICategorieDao {
 
 	@Override
 	public Categorie getCategorie(Categorie categorie) {
+		Session s = sf.getCurrentSession();
+		
 		String req = "select cat from Categorie cat where cat.idCategorie=:pIdCat";
 		
-		Query query = em.createQuery(req);
+		Query query = s.createQuery(req);
 		query.setParameter("pIdCat", categorie.getIdCategorie());
 		
-		categorie = (Categorie) query.getSingleResult();
-		System.out.println("------------------------- "+categorie);
+		categorie = (Categorie) query.uniqueResult();
 		return categorie;
 		
 	}
 
 	@Override
 	public List<Categorie> getAllCategorie() {
+		Session s = sf.getCurrentSession();
+		
 		// Construction de la requete
 		String req = "SELECT cat FROM Categorie cat";
 
-		Query query = em.createQuery(req);
+		Query query = s.createQuery(req);
 
-		List<Categorie> listeCat = query.getResultList();
+		List<Categorie> listeCat = query.list();
 
 		return listeCat;
 
--- 11_eCommerce_JSF_Spring_Hibernate/src/main/java/fr/adaming/dao/ProduitDaoImpl.java
+++ 11_eCommerce_JSF_Spring_Hibernate/src/main/java/fr/adaming/dao/ProduitDaoImpl.java
@@ -2,10 +2,10 @@ package fr.adaming.dao;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import fr.adaming.model.Categorie;
@@ -14,63 +14,55 @@ import fr.adaming.model.Produit;
 @Repository
 public class ProduitDaoImpl implements IProduitDao {
 
-	@PersistenceContext(unitName = "PU")
-	private EntityManager em;
+	@Autowired
+	private SessionFactory sf;
+
+	// Setter pour l'injection de d�pendance
+	public void setSf(SessionFactory sf) {
+		this.sf = sf;
+	}
 
 	@Override
 	public Produit produitExists(Produit produit) {
+		Session s = sf.getCurrentSession();
+
 		String req = "SELECT p FROM Produit p WHERE p.designation=:pDesignation";
 
-		Query query = em.createQuery(req);
+		Query query = s.createQuery(req);
 		query.setParameter("pDesignation", produit.getDesignation());
 
-		Produit outProduit = (Produit) query.getSingleResult();
+		Produit outProduit = (Produit) query.uniqueResult();
 
-		if (outProduit != null) {
-			return outProduit;
-		} else {
-			return null;
-		}
+		return outProduit;
 	}
 
 	@Override
 	public Produit addProduit(Produit produit) {
-		em.persist(produit);
-		return produit;
+		Session s = sf.getCurrentSession();
 
-		// Cette méthode renvoie soit le produit créé, soit un produit déjà
-		// existant avec la même désignation
-		// Produit checkProduit = produitExists(produit);
-		// System.out.println("----------------------" +checkProduit);
-		// if (checkProduit!=null) {
-		// return checkProduit;
-		// } else {
-		// em.persist(produit);
-		// return produit;
-		// }
+		s.save(produit);
+
+		return produit;
 	}
 
 	@Override
 	public Produit updateProduit(Produit produit) {
-		// la fonction vérifie que le produit existant est bien différent de
-		// celui donné
-		// retour de null si la mise à jour n'a pas eu lieu car le produit
-		// n'est pas différent
-		Produit prodUpdate = em.find(Produit.class, produit.getIdProduit());
 
-		if (!prodUpdate.equals(produit)) {
-			em.merge(produit);
-			return produit;
-		} else {
-			return null;
-		}
+		Session s = sf.getCurrentSession();
+
+		s.update(produit);
+
+		return produit;
 	}
 
 	@Override
 	public boolean deleteProduit(Produit produit) {
+
+		Session s = sf.getCurrentSession();
+
 		String req = "DELETE from Produit prod WHERE prod.idProduit=:pIdProd";
 
-		Query query = em.createQuery(req);
+		Query query = s.createQuery(req);
 		query.setParameter("pIdProd", produit.getIdProduit());
 
 		int verif = query.executeUpdate();
@@ -84,33 +76,31 @@ public class ProduitDaoImpl implements IProduitDao {
 
 	@Override
 	public Produit getProduit(Produit produit) {
+		Session s = sf.getCurrentSession();
 		// on trouve le produit par son ID, puis on renvoie le résultat, ou
 		// null
-		Produit outProduit = em.find(Produit.class, produit.getIdProduit());
-		if (!outProduit.equals(null)) {
-			return outProduit;
-		} else {
-			return null;
-		}
+		Produit outProduit = (Produit) s.get(Produit.class, produit.getIdProduit());
+
+		return outProduit;
 	}
 
 	@Override
 	public List<Produit> getProduitSelected() {
-		String request = "SELECT p FROM Produit p WHERE p.selectionne=:pSelect";
+		Session s = sf.getCurrentSession();
+		String req = "SELECT p FROM Produit p WHERE p.selectionne=:pSelect";
 
-		Query query = em.createQuery(request);
+		Query query = s.createQuery(req);
 		query.setParameter("pSelect", true);
 
-		List<Produit> result = (List<Produit>) query.getResultList();
-		if (result.isEmpty() || result.get(0).equals(null)) {
-			return null;
-		} else {
-			return result;
-		}
+		List<Produit> liste = query.list();
+
+		return liste;
+
 	}
 
 	@Override
 	public List<Produit> getProduitByKW(String[] keywords, boolean allKWs) {
+		Session s = sf.getCurrentSession();
 
 		StringBuilder keywordsPart = new StringBuilder();
 		String connector;
@@ -129,46 +119,49 @@ public class ProduitDaoImpl implements IProduitDao {
 		}
 
 		String request = "SELECT p from Produit p WHERE " + keywordsPart.toString();
-		Query query = em.createQuery(request);
+		Query query = s.createQuery(request);
 
-		List<Produit> result = (List<Produit>) query.getResultList();
+		List<Produit> liste = query.list();
 
-		if (result.isEmpty() || result.get(0).equals(null)) {
+		if (liste.isEmpty() || liste.get(0).equals(null)) {
 			return null;
 		} else {
-			return result;
+			return liste;
 		}
 
 	}
 
 	@Override
 	public List<Produit> getProductByCategorie(Categorie categorie) {
-		String request = "SELECT p FROM Produit p WHERE p.categorie.idCategorie=:pCategorie";
+		Session s = sf.getCurrentSession();
 
-		Query query = em.createQuery(request);
+		String req = "SELECT p FROM Produit p WHERE p.categorie.idCategorie=:pCategorie";
+
+		Query query = s.createQuery(req);
 		query.setParameter("pCategorie", categorie.getIdCategorie());
 
-		List<Produit> result = (List<Produit>) query.getResultList();
-		if (result.isEmpty() || result.get(0).equals(null)) {
+		List<Produit> liste = query.list();
+		if (liste.isEmpty() || liste.get(0).equals(null)) {
 			return null;
 		} else {
-			return result;
+			return liste;
 		}
 	}
 
 	@Override
 	public List<Produit> getAllProduit() {
-		String request = "SELECT p from Produit p";
+		Session s = sf.getCurrentSession();
 
-		Query query = em.createQuery(request);
+		String req = "SELECT p from Produit p";
 
-		List<Produit> result = (List<Produit>) query.getResultList();
+		Query query = s.createQuery(req);
 
-		if (result.isEmpty() || result.get(0).equals(null)) {
+		List<Produit> liste = query.list();
+
+		if (liste.isEmpty() || liste.get(0).equals(null)) {
 			return null;
 		} else {
-			return result;
+			return liste;
 		}
 	}
-
 }
